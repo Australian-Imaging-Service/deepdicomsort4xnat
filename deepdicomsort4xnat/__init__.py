b8eb9b7e4acfaa23e2087e1acfe902cd9f413b1e
Thomas G. Close <tom.g.close@gmail.com>
2022-05-02 09:40:23 +1000

    initial framework for package

---
+++ deepdicomsort4xnat/__init__.py
@@ -0,0 +1,2 @@
+from .workflow import dds4xnat_workflow
+from .tasks import rename_on_xnat
---
+++ deepdicomsort4xnat/tasks.py
@@ -0,0 +1,16 @@
+from pydra import mark
+import xnat
+
+
+@mark.task
+def rename_on_xnat(server_url: str,
+                   project_id: str,
+                   session_id: str,
+                   username: str,
+                   password: str,
+                   name_mapping_file: str):
+    
+    with xnat.connect(server=server_url, user=username, password=password) as xlogin:
+        xproject = xlogin.projects[project_id]
+        xsession = xproject.experiments[session_id]
+    raise NotImplementedError
